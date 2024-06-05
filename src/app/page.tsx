2432302ffc32f68c5c9e8ea2b9cbc7149655b7bb
yokonam <s.yokonami@gmail.com>
2024-06-05 19:04:26 +0900

    feat: Add configuration files and utility functions
    The code changes include adding several configuration files and a utility function. This commit introduces the following changes:
    
    - Added `.eslintrc.json` file with the `next/core-web-vitals` extension.
    - Added `next.config.mjs` file with an empty `NextConfig` object.
    - Added `postcss.config.mjs` file with the `tailwindcss` plugin.
    - Added `tsconfig.json` file with various compiler options.
    - Added `components.json` file with configuration for the UI components.
    - Added `.gitignore` file with common ignore patterns.
    - Added `public/vercel.svg` file with the Vercel logo.
    - Added `src/lib/utils.ts` file with the `cn` utility function.
    - Added `src/app/layout.tsx` file with the `RootLayout` component.
    - Updated `package.json` file with dependencies and scripts.
    - Added `src/components/ui/separator.tsx` file with the `Separator` component.
    
    These changes are necessary to configure the project and provide utility functions for future development.

---
+++ src/app/page.tsx
@@ -0,0 +1,183 @@
+'use client'
+import { useState } from 'react'
+import { Button } from '@/components/ui/button'
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle
+} from '@/components/ui/card'
+import { Separator } from '@/components/ui/separator'
+
+type CardData = {
+  title: string
+  status: boolean[]
+}
+
+type Cards = {
+  [key: string]: CardData
+}
+
+export default function Home() {
+  const initCards: Cards = {
+    trapA: { title: '🐍', status: [false, false, false] },
+    trapB: { title: '🕷️', status: [false, false, false] },
+    trapC: { title: '🔥', status: [false, false, false] },
+    trapD: { title: '🪨', status: [false, false, false] },
+    trapE: { title: '🧟‍♂️', status: [false, false, false] }
+  }
+
+  const [trapCards, setTrapCards] = useState<Cards>(initCards)
+  const [otherCards, setOtherCards] = useState<number>(15)
+
+  const updateTrapCardStatus = (cardName: keyof Cards, type: string) => {
+    const updatedCards = { ...trapCards }
+    const cardIndexes = updatedCards[cardName].status
+
+    if (type === 'plus') {
+      const index = cardIndexes.indexOf(false)
+      if (index !== -1) {
+        cardIndexes[index] = true
+      }
+    } else if (type === 'minus') {
+      const index = cardIndexes.lastIndexOf(true)
+      if (index !== -1) {
+        cardIndexes[index] = false
+      }
+    }
+
+    setTrapCards(updatedCards)
+  }
+
+  const handleOtherCardsChange = (type: string) => {
+    if (type === 'plus' && otherCards > 0) {
+      setOtherCards(otherCards - 1)
+    } else if (type === 'minus' && otherCards < 15) {
+      setOtherCards(otherCards + 1)
+    }
+  }
+
+  const getRemainingTrapCards = () => {
+    const remaining: { cardName: string; index: number }[] = []
+    Object.entries(trapCards).forEach(([cardName, cardData]) => {
+      cardData.status.forEach((checked, index) => {
+        if (!checked) {
+          remaining.push({ cardName, index })
+        }
+      })
+    })
+    return remaining
+  }
+
+  const calculateProbabilities = (
+    remainingCards: { cardName: string; index: number }[]
+  ) => {
+    const total = remainingCards.length + otherCards
+    const probabilities: { [key: string]: number } = {}
+
+    remainingCards.forEach(({ cardName }) => {
+      probabilities[cardName] = (probabilities[cardName] || 0) + 1 / total
+    })
+
+    probabilities['otherCards'] = otherCards / total
+    return probabilities
+  }
+
+  const countFalse = (array: boolean[]) =>
+    array.filter((value) => !value).length
+
+  const remainingTrapCards = getRemainingTrapCards()
+  const probabilities = calculateProbabilities(remainingTrapCards)
+  const totalCards = remainingTrapCards.length + otherCards
+  const trapCardsProbability = remainingTrapCards.length / totalCards
+
+  return (
+    <div className={'grid grid-cols-[1fr_3fr] gap-4 p-5 bg-slate-50'}>
+      <div className={'grid grid-rows-5 gap-4'}>
+        {Object.entries(trapCards).map(([cardName, cardData], index) => (
+          <Card key={`${cardName}-${index}`}>
+            <CardHeader>
+              <CardTitle className={'text-5xl flex gap-4'}>
+                {cardData.title}
+                <div className={'flex gap-2 items-center'}>
+                  <Button
+                    variant="outline"
+                    size="icon"
+                    className={'h-5'}
+                    onClick={() =>
+                      updateTrapCardStatus(cardName as keyof Cards, 'minus')
+                    }
+                  >
+                    -
+                  </Button>
+                  <Button
+                    variant="outline"
+                    size="icon"
+                    className={'h-5'}
+                    onClick={() =>
+                      updateTrapCardStatus(cardName as keyof Cards, 'plus')
+                    }
+                  >
+                    +
+                  </Button>
+                </div>
+              </CardTitle>
+            </CardHeader>
+            <CardContent>
+              <p>Remaining: {countFalse(cardData.status)}</p>
+              <p>
+                Probabilities: {(probabilities[cardName] * 100).toFixed(2)}%
+              </p>
+            </CardContent>
+          </Card>
+        ))}
+      </div>
+      <div className={'grid grid-cols-1 gap-4'}>
+        <Card>
+          <CardHeader>
+            <CardTitle>Other Cards</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <p>Count: {otherCards}</p>
+          </CardContent>
+          <CardFooter className={'gap-2'}>
+            <Button
+              variant="outline"
+              size="icon"
+              className={'h-5'}
+              onClick={() => handleOtherCardsChange('minus')}
+            >
+              -
+            </Button>
+            <Button
+              variant="outline"
+              size="icon"
+              className={'h-5'}
+              onClick={() => handleOtherCardsChange('plus')}
+            >
+              +
+            </Button>
+          </CardFooter>
+        </Card>
+
+        <Card>
+          <CardHeader>
+            <CardTitle>Summary</CardTitle>
+          </CardHeader>
+
+          <CardContent>
+            <p>Total trapCards: {remainingTrapCards.length}</p>
+            <p>Total otherCards: {otherCards}</p>
+            <p>Total cards: {totalCards}</p>
+            <p>
+              Probability of any trapCard:{' '}
+              {(trapCardsProbability * 100).toFixed(2)}%
+            </p>
+          </CardContent>
+        </Card>
+      </div>
+    </div>
+  )
+}
